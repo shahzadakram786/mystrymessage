6523117660008b88fdc87fb7376fa96b1ecc4bdb
shahzadakram786 <alyakram786@gmail.com>
2025-07-23 16:35:39 +0500

    connection with database

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,14 @@
+import {z} from 'zod';
+
+
+export const messageSchema = z.object({
+
+    content: z
+    .string()
+    .min(10, { message: "Message must be at least 10 characters long" })
+    .max(300, { message: "Message must be at most 500 characters long" }),
+
+
+
+   
+})
